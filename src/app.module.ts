22d066f06d2c4ac908c5554e023241cddb9e9b05
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-11 11:18:18 +0100

    feat: init Borrowers, borrows, employees

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BorrowersModule } from './borrowers/borrowers.module';
+import { EmployeesModule } from './employees/employees.module';
+import { BorrowsModule } from './borrows/borrows.module';
 
 @Module({
-  imports: [],
+  imports: [BorrowersModule, EmployeesModule, BorrowsModule],
   controllers: [AppController],
   providers: [AppService],
 })

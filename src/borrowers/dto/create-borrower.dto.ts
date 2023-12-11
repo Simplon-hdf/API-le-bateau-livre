07f18c5d24bb58133e12661ee798d7db786f7997
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-11 13:58:45 +0100

    feat: ajout de borrower dto

--- src/borrowers/dto/create-borrower.dto.ts
+++ src/borrowers/dto/create-borrower.dto.ts
@@ -1,1 +1,23 @@
-export class CreateBorrowerDto {}
+import { IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBorrowerDto {
+
+    @ApiProperty({
+        description: 'This field represents the employee first name',
+        minLength: 3,
+        maxLength: 20,
+      })
+    @IsString()
+    @Length(3, 20)
+    first_name : string;
+
+    @ApiProperty({
+        description: 'This field represents the employee last name',
+        minLength: 2,
+        maxLength: 30,
+      })
+    @IsString()
+    @Length(2, 30)
+    last_name : string;
+}

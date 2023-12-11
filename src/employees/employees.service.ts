49140ea61e81c8350754eda14ea07d9d9fab88c5
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-11 12:20:23 +0100

    feat: Normalized employee

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { PrismaService } from 'src/prisma.service';
+import NormalizedResponse from 'src/utils/normalized-response';
 
 @Injectable()
 export class EmployeesService {
   constructor(private readonly prisma: PrismaService) {}
 
   public async create(createEmployeeDto: CreateEmployeeDto) {
-    const createdEmployee = await this.prisma.employees.create({
+    const createdEmployee = new NormalizedResponse(
+      `Employee ${createEmployeeDto.first_name} has been created`,
+      await this.prisma.employees.create({
       data: {
         mail_address: createEmployeeDto.mail_address,
         password: createEmployeeDto.password,
@@ -16,8 +19,9 @@ export class EmployeesService {
         last_name: createEmployeeDto.last_name,
 
       },
-    });
-    return createdEmployee;
+    }),
+  );
+    return createdEmployee.toJSON;
   }
 
   findAll() {
@@ -25,16 +29,21 @@ export class EmployeesService {
   }
 
   public async getByUUID(uuid: string) {
-    const gettedEmployee = await this.prisma.employees.findUnique({
+    const gettedEmployee = new NormalizedResponse(
+      `Employee ${uuid} has been found`,
+      await this.prisma.employees.findUnique({
       where: {
         UUID: uuid,
       },
-    });
-    return gettedEmployee;
+    }),
+  );
+    return gettedEmployee.toJSON;
   }
 
   public async updateByUUID(uuid: string, updateEmployeeDto: UpdateEmployeeDto) {
-    const updatedEmployee = await this.prisma.employees.update({
+    const updatedEmployee = new NormalizedResponse(
+      `Employee ${updateEmployeeDto.first_name} has been updated`,
+      await this.prisma.employees.update({
       where: {
         UUID: uuid,
       },
@@ -44,16 +53,20 @@ export class EmployeesService {
         first_name: !!updateEmployeeDto.first_name ? updateEmployeeDto.first_name : undefined,
         last_name: !!updateEmployeeDto.last_name ? updateEmployeeDto.last_name : undefined,
       },
-    });
-    return updatedEmployee;
+    }),
+  );
+    return updatedEmployee.toJSON;
   }
 
   public async deleteByUUID(uuid: string) {
-    const deletedEmployee = await this.prisma.employees.delete({
+    const deletedEmployee = new NormalizedResponse(
+      `Employee ${uuid} has been deleted`,
+      await this.prisma.employees.delete({
       where: {
         UUID: uuid,
       },
-    });
-    return deletedEmployee;
+    }),
+  );
+    return deletedEmployee.toJSON;
   }
 }

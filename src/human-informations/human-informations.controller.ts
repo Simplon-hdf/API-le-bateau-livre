30cf000c5996d60c853f2118fe83b8e6ae8f72f1
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-12 10:53:15 +0100

    feat: ajout du controller human information

--- src/human-informations/human-informations.controller.ts
+++ src/human-informations/human-informations.controller.ts
@@ -2,8 +2,11 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { HumanInformationsService } from './human-informations.service';
 import { CreateHumanInformationDto } from './dto/create-human-information.dto';
 import { UpdateHumanInformationDto } from './dto/update-human-information.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('human-informations')
+@ApiTags('human-informations')
+
 export class HumanInformationsController {
   constructor(private readonly humanInformationsService: HumanInformationsService) {}
 
@@ -17,18 +20,21 @@ export class HumanInformationsController {
     return this.humanInformationsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.humanInformationsService.findOne(+id);
+  @Get(':uuid')
+  public getByUUID(@Param('uuid') uuid: string) {
+    return this.humanInformationsService.getByUUID(uuid);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateHumanInformationDto: UpdateHumanInformationDto) {
-    return this.humanInformationsService.update(+id, updateHumanInformationDto);
+  @Patch(':uuid')
+  public updateByUUID(
+    @Param('uuid') uuid: string,
+    @Body() updateUserDto: UpdateHumanInformationDto,
+  ) {
+    return this.humanInformationsService.updateByUUID(uuid, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.humanInformationsService.remove(+id);
+  @Delete(':uuid')
+  public deleteByUUID(@Param('uuid') uuid: string) {
+    return this.humanInformationsService.deleteByUUID(uuid);
   }
 }

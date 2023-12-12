9a3a4401538d9f7da0d86d3b35bda49ebb49ad1f
writememory1337 <147529830+writememory1337@users.noreply.github.com>
2023-12-12 15:50:30 +0100

    fix: edited book dto

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -2,16 +2,28 @@ import { IsEmail, IsString, Length, MaxLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookDto {
-    @ApiProperty({  
+    @ApiProperty({
         description: 'Book title',})
     @IsEmail()
     @Length(1, 40)
-    name : string;
+    name: string;
 
     @ApiProperty({
-    description: 'Book description',})
+        description: 'Book description',})
     @IsString()
     @Length(1, 500)
-    password : string;
+    description: string;
+
+
+    @ApiProperty({
+        description: "The book's author_UUID",})
+    @IsString()
+    public author_UUID: string;
+
+    @ApiProperty({
+        description: "The book's borrow_UUID",})
+    @IsString()
+    public borrow_UUID: string;
 }
+
 

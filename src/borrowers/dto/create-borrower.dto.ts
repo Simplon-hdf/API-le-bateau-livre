52e7b9e9214c0cb3c7a194d5115a260d8a8ff0bf
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-12 10:13:43 +0100

    fix DTO employee et borrower

--- src/borrowers/dto/create-borrower.dto.ts
+++ src/borrowers/dto/create-borrower.dto.ts
@@ -3,21 +3,5 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBorrowerDto {
 
-    @ApiProperty({
-        description: 'This field represents the employee first name',
-        minLength: 3,
-        maxLength: 20,
-      })
-    @IsString()
-    @Length(3, 20)
-    first_name : string;
 
-    @ApiProperty({
-        description: 'This field represents the employee last name',
-        minLength: 2,
-        maxLength: 30,
-      })
-    @IsString()
-    @Length(2, 30)
-    last_name : string;
 }

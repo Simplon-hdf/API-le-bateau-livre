f3200ed612448a779896fef7d23cdd916501fedb
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-13 10:40:01 +0100

    fix: borrower dto et borrower controller rectification

--- src/borrowers/dto/create-borrower.dto.ts
+++ src/borrowers/dto/create-borrower.dto.ts
@@ -2,5 +2,15 @@ import { IsString, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBorrowerDto {
-    public humanInformation_UUID : string;
+    @ApiProperty({
+        description: 'This field represents the employee first name',
+      })
+    @IsString()
+    first_name: string;
+
+    @ApiProperty({
+        description: 'This field represents the employee last name',
+      })
+    @IsString()
+    last_name: string;
 }
